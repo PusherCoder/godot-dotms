d77f4097f5a9e822ff7e98693a4cab0b2472aba9
PusherCoder <bendegn@gmail.com>
2019-01-27 15:19:01 -0800

    Fixed Movement
    Movement now smoothly transitions between tiles. Converted over to using WASDQE for primary movement and menuing options.

--- src/Area2D.gd
+++ src/Area2D.gd
@@ -9,25 +9,27 @@ func _ready():
 	screensize = get_viewport_rect().size
 	#hide()
 
-func _process(delta):
-	if Input.is_action_pressed("ui_up"):
-		position.x -= 6
-		position.y -= 6
-	if Input.is_action_pressed("ui_down"):
-		position.x -= 6
-		position.y += 6
-	if Input.is_action_pressed("ui_left"):
-		position.x -= 12
-	if Input.is_action_pressed("ui_right"):
-		position.x += 12
-	if Input.is_action_pressed("ui_page_up"):
-		position.x += 6
-		position.y -= 6
-	if Input.is_action_pressed("ui_page_down"):
-		position.x += 6
-		position.y += 6
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		position.y += 48
+		position.x -= 83
+	if event.is_action_pressed("ui_right"):
+		position.y += 48
+		position.x += 83
+	if event.is_action_pressed("ui_down"):
+		position.y += 96
+	if event.is_action_pressed("ui_up"):
+		position.y -= 96
+	if event.is_action_pressed("ui_page_down"):
+		position.y -= 48
+		position.x -= 83
+	if event.is_action_pressed("ui_page_up"):
+		position.y -= 48
+		position.x += 83
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+
+#func _process(delta):
 
 func _on_Player_body_entered(body):
 	#hide()
