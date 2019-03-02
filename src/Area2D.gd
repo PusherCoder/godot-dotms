451a7d5457b9bfee80ebfec9941fb13fae73d670
PusherCoder <bendegn@gmail.com>
2019-03-02 10:30:18 -0800

    Fixed Movement
    Going with plan to have each movement token contain a hit box. Those hit boxes will interact with the adjacent cells and give a prediction as to what action can be taken in each cell.

--- src/Area2D.gd
+++ src/Area2D.gd
@@ -4,32 +4,50 @@ signal hit
 
 export (int) var speed
 var screensize
+var opacity
+var blink_direction
 
 func _ready():
+	opacity = 1.0
+	blink_direction = 0
 	screensize = get_viewport_rect().size
 	#hide()
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
-		position.y += 48
-		position.x -= 83
+		position.x -= 82
+		position.y += 47
 	if event.is_action_pressed("ui_right"):
-		position.y += 48
-		position.x += 83
+		position.x += 82
+		position.y += 47
 	if event.is_action_pressed("ui_down"):
-		position.y += 96
+		position.y += 94
 	if event.is_action_pressed("ui_up"):
-		position.y -= 96
+		position.y -= 94
 	if event.is_action_pressed("ui_page_down"):
-		position.y -= 48
-		position.x -= 83
+		position.x -= 82
+		position.y -= 47
 	if event.is_action_pressed("ui_page_up"):
-		position.y -= 48
-		position.x += 83
+		position.x += 82
+		position.y -= 47
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
 
-#func _process(delta):
+func _process(delta):
+	if( opacity < blink_direction ):
+		opacity += 0.01
+	if( opacity > blink_direction ):
+		opacity -= 0.01
+	if( opacity < 0.1 ):
+		blink_direction = 1
+	if( opacity > 0.9 ):
+		blink_direction = 0
+	$QSprite.modulate = Color(1,1,1,opacity)
+	$WSprite.modulate = Color(1,1,1,opacity)
+	$ESprite.modulate = Color(1,1,1,opacity)
+	$ASprite.modulate = Color(1,1,1,opacity)
+	$SSprite.modulate = Color(1,1,1,opacity)
+	$DSprite.modulate = Color(1,1,1,opacity)
 
 func _on_Player_body_entered(body):
 	#hide()
