005fef03fc8455c4d754c74219067042bfc8796e
PusherCoder <bendegn@gmail.com>
2019-04-10 20:15:14 -0700

    Collision Now Works on All Surfaces
    Next working on predicting out all possible movements on a given turn.

--- src/scenes/battle/battle_character_test.gd
+++ src/scenes/battle/battle_character_test.gd
@@ -1,8 +1,6 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var movement
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,21 +9,38 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("key_up"):
-		position.y -= 100
-		position.x -= 90
-		get_node("AnimatedSprite").set_flip_h( true )
+		get_node("RayCast2D").set_cast_to( Vector2( -90, -100 ) )
+		movement = 1
 	elif event.is_action_pressed("key_down"):
-		position.y += 100
-		position.x += 90
-		get_node("AnimatedSprite").set_flip_h( false )
+		get_node("RayCast2D").set_cast_to( Vector2( 90, 100 ) )
+		movement = 2
 	elif event.is_action_pressed("key_right"):
-		position.x += 90
-		get_node("AnimatedSprite").set_flip_h( false )
+		get_node("RayCast2D").set_cast_to( Vector2( 90, 0 ) )
+		movement = 3
 	elif event.is_action_pressed("key_left"):
-		position.x -= 90
-		get_node("AnimatedSprite").set_flip_h( true )
+		get_node("RayCast2D").set_cast_to( Vector2( -90, 0 ) )
+		movement = 4
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if movement == 1:
+		movement = 0
+		if get_node("RayCast2D").is_colliding() == false:
+			position.y -= 100
+			position.x -= 90
+			get_node("AnimatedSprite").set_flip_h( true )
+	elif movement == 2:
+		movement = 0
+		if get_node("RayCast2D").is_colliding() == false:
+			position.y += 100
+			position.x += 90
+			get_node("AnimatedSprite").set_flip_h( false )
+	elif movement == 3:
+		movement = 0
+		if get_node("RayCast2D").is_colliding() == false:
+			position.x += 90
+			get_node("AnimatedSprite").set_flip_h( false )
+	elif movement == 4:
+		movement = 0
+		if get_node("RayCast2D").is_colliding() == false:
+			position.x -= 90
+			get_node("AnimatedSprite").set_flip_h( true )
